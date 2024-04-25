51e2ce4aa50e84203d0fd35b7ed3e9cfe5aa571c
Soukaina <battahsoukaina@gmail.com>
2024-04-25 11:08:36 +0100

    premier

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
---
+++ app/src/main/java/com/example/myapplication/acceuilActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class acceuilActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}

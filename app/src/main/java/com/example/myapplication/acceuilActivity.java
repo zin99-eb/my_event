d3ca6826439ef81d1758ffa9ec3d9c69da96e2f0
Soukaina <battahsoukaina@gmail.com>
2024-04-25 16:20:01 +0100

    commetaire22

--- app/src/main/java/com/example/myapplication/acceuilActivity.java
+++ app/src/main/java/com/example/myapplication/acceuilActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class acceuilActivity extends AppCompatActivity {
-
+//comm
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
